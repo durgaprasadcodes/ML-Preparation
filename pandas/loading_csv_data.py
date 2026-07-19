51151bce10b297cd02a0fa79347f08e0ccee829b
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-07-19 11:53:56 +0530

    Loading CSV Datasets from Kaggle Datasets

--- pandas/loading_csv_data.py
+++ pandas/loading_csv_data.py
@@ -2,23 +2,23 @@ import pandas as pd
 
 data = pd.read_csv(r"C:\Users\rolex\OneDrive\Desktop\ML Preparation\pandas\heart_disease_risk_2026.csv")
 
-# print(data)
+print(data)
 
 new_data = data.iloc[:10,:5]
 
-# print(new_data)
-# print(new_data[["patient_id","age"]])
-# print(new_data[new_data["age"]>55])
-# print(new_data["age"]>55)
+print(new_data)
+print(new_data[["patient_id","age"]])
+print(new_data[new_data["age"]>55])
+print(new_data["age"]>55)
 
 # Dataset Inspection ⭐⭐
 
-# print(new_data.head())
-# print(new_data.tail())
-# print(new_data.shape)
-# print(new_data.columns)
-# print(new_data.describe())
-# print(new_data.info())
+print(new_data.head())
+print(new_data.tail())
+print(new_data.shape)
+print(new_data.columns)
+print(new_data.describe())
+print(new_data.info())
 
 # Value Counts ⭐⭐
 
