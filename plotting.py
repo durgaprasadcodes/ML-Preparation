682a37d4ebb70b5310bb8d5468ccf83be5c1619e
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-08-16 22:39:58 +0530

    Seaborn replot plotting

---
+++ plotting.py
@@ -0,0 +1,16 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tips = sns.load_dataset('tips')
+
+sns.relplot(
+    data=tips,
+    x='total_bill',
+    y='tip',
+    col='time',
+    hue='smoker',
+    style='smoker',
+    size='size'
+)
+
+plt.show()
