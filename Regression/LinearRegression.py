e3375b1b49151494b54ddf3060b4fb616cc0c32d
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-09-15 12:34:17 +0530

    First Regression Model - Linear Regression ⚡

--- Regression/LinearRegression.py
+++ Regression/LinearRegression.py
@@ -1,21 +1,27 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+from babel.numbers import format_currency
 
 dataset = pd.read_csv(r"C:\Users\rolex\OneDrive\Desktop\ML Preparation\Regression\house_price_dataset.csv")
 
 df = pd.DataFrame(dataset)
 
-X,y = df.drop("price",axis=1),df["price"]
+X, y = df.drop("price", axis=1), df["price"]
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.3,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=42)
 
 model = LinearRegression()
 
-model.fit(X_train,y_train)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
 
 
 new_house = pd.DataFrame([{
@@ -27,8 +33,32 @@ new_house = pd.DataFrame([{
     "distance_to_city_km": 0
 }])
 
-print(model.predict(new_house))
+prediction = model.predict(new_house)[0]
 
+formatted = format_currency(
+    prediction,
+    "INR",
+    locale="en_IN"
+)
+
+print("Predicted Price:", formatted)
+
+min_value = min(y_test.min(), y_pred.min())
+max_value = max(y_test.max(), y_pred.max())
+
+plt.scatter(y_test, y_pred, color="r", alpha=.5)
+
+plt.title("House Price Prediction")
+plt.xlabel("Actual Price")
+plt.ylabel("Predicted Price")
+
+plt.plot(
+    [min_value, max_value],
+    [min_value, max_value],
+    linestyle="--"
+)
+
+plt.show()
 
 # y_pred = model.predict(X_test)
 
